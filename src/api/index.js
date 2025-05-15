49ca5dbb3f284def69f936e989cb031bb559bab2
clavijo99 <bclavijogomez@gmail.com>
2025-05-14 23:39:38 -0500

    Fix: Update environment variable for API URL to NEXT_PUBLIC_API_HOST

--- src/api/index.js
+++ src/api/index.js
@@ -4,7 +4,8 @@
 
 import { AuthenticationApi, CarouselApi} from '../api-client/';
 import axiosInstance from "./axios";
-const apiUrl = process.env.API_HOST || "http://192.168.1.12:8000";
+
+const apiUrl = process.env.NEXT_PUBLIC_API_HOST || "http://192.168.1.12:8000";
 
 export const authenticateApi = new AuthenticationApi(undefined, apiUrl, axiosInstance);
 export const carouselApi = new CarouselApi(undefined, apiUrl, axiosInstance);
