46359293cc977827ebc94fbdff0ba7f5da690e64
clavijo99 <60893557+clavijo99@users.noreply.github.com>
2025-05-18 21:35:04 -0500

    Feature/validate server (#39)
    * Feature: Implement server validation and add mainApi integration
    
    * Feature: Add mainPingRetrieve endpoint and update related configurations
    
    * remove comment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,15 @@
 'use client';
 
+import { useEffect, useState } from 'react';
 import React from 'react';
 import { usePathname } from 'next/navigation';
+
+import './globals.css';
+
 import Header from '../components/common/Header';
 import Footer from '../components/common/Footer';
-import './globals.css';
+
+import { mainApi } from '../api';
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   const pathname = usePathname();
@@ -18,11 +23,27 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
 
   const shouldHide = hideHeaderFooter.includes(pathname);
 
+  const [isServerReady, setIsServerReady] = useState(false);
+
+  useEffect(() => {
+    const checkServer = async () => {
+      try {
+        await mainApi.mainPingRetrieve();
+        setIsServerReady(true);
+      } catch (error) {
+        console.log('Servidor no disponible, reintentando en 3s...');
+        setTimeout(checkServer, 3000);
+      }
+    };
+
+    checkServer();
+  }, []);
+
   return (
     <html lang='es' suppressHydrationWarning>
       <head>
         <title>Fantasía Store</title>
-        <meta name="description" content="Tienda de ropa y accersorios"/>
+        <meta name="description" content="Tienda de ropa y accesorios" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="theme-color" content="#F7E9E9" />
         
@@ -33,9 +54,18 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
 
 
 
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet" />
+        <link rel="icon" href="/favicon-clothe.png" />
       </head>
-
       <body>
+        {!isServerReady && (
+          <div className="fixed inset-0 z-50 bg-white/80 backdrop-blur-md flex items-center justify-center flex-col">
+            <div className="text-center mb-4">
+              <p className="text-xl font-medium">Estableciendo Conexion Con El  Servidor</p>
+            </div>
+              <div className="w-12 h-12 border-4 border-dashed rounded-full animate-spin border-black-400 border-t-transparent"></div>
+          </div>
+        )}
         <div className='min-h-screen flex flex-col'>
           {/* Header */}
           {!shouldHide && <Header />}
