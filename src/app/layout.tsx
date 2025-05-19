23d274309b8d66eb986fcfd92e08ed866c97e83d
Brayan Garzon <destroyergarzon@gmail.com>
2025-05-18 22:14:58 -0500

    Add component whatsApp

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,66 @@
+// 'use client';
+
+// import React from 'react';
+// import { usePathname } from 'next/navigation';
+// import Header from '../components/common/Header';
+// import Footer from '../components/common/Footer';
+// import WhatsAppFloat from '@/components/common/whatsAppFloat';
+// import './globals.css';
+
+// export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+//   const pathname = usePathname();
+
+//   // Rutas donde NO quieres mostrar Header/Footer
+//   const hideHeaderFooter = [
+//     '/mision',
+//     '/vision',
+//     '/politicas'
+//   ];
+
+//   const shouldHide = hideHeaderFooter.includes(pathname);
+
+//   return (
+//     <html lang='es' suppressHydrationWarning>
+//       <head>
+//         <title>Fantasía Store</title>
+//         <meta name="description" content="Tienda de ropa y accersorios"/>
+//         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+//         <meta name="theme-color" content="#F7E9E9" />
+        
+        
+//         <link rel="icon" href="/Store_Favicon.png" sizes="32x32" type="image/png" />
+//         <link rel="icon" href="/Store_Favicon.png" sizes="64x64" type="image/png" />
+//         <link rel="icon" href="/Store_Favicon.png" sizes="128x128" type="image/png" />
+
+
+
+//       </head>
+
+//       <body>
+//         <div className='min-h-screen flex flex-col'>
+//           {/* Header */}
+//           {!shouldHide && <Header />}
+
+//           {/* Contenido Principal */}
+//           <main className='flex-grow'>{children}</main>
+
+//           {/* Footer */}
+//           {!shouldHide && <Footer />}
+//         </div>
+//         {/* Botón de WhatsApp */}
+//         <WhatsAppFloat />
+//       </body>
+//     </html>
+//   );
+// }
+
 'use client';
 
 import React from 'react';
 import { usePathname } from 'next/navigation';
 import Header from '../components/common/Header';
 import Footer from '../components/common/Footer';
+import WhatsAppFloat from '@/components/common/whatsAppFloat';
 import './globals.css';
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
@@ -22,31 +79,25 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
     <html lang='es' suppressHydrationWarning>
       <head>
         <title>Fantasía Store</title>
-        <meta name="description" content="Tienda de ropa y accersorios"/>
+        <meta name="description" content="Tienda de ropa y accesorios"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="theme-color" content="#F7E9E9" />
-        
-        
         <link rel="icon" href="/Store_Favicon.png" sizes="32x32" type="image/png" />
         <link rel="icon" href="/Store_Favicon.png" sizes="64x64" type="image/png" />
         <link rel="icon" href="/Store_Favicon.png" sizes="128x128" type="image/png" />
-
-
-
       </head>
 
       <body>
         <div className='min-h-screen flex flex-col'>
-          {/* Header */}
           {!shouldHide && <Header />}
-
-          {/* Contenido Principal */}
           <main className='flex-grow'>{children}</main>
-
-          {/* Footer */}
           {!shouldHide && <Footer />}
         </div>
+
+        {/* Botón de WhatsApp - Fuera del contenedor flexible */}
+        <WhatsAppFloat />
       </body>
+
     </html>
   );
 }
